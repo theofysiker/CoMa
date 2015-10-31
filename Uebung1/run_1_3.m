8d20d53b06bacc8a06f88e9aaa1f2290193bfdbd
Nicolas Lehmann <mail@nicolaslehmann.de>
2015-10-31 21:59:30 +0100

    dual1 fertig

--- Uebung1/run_1_3.m
+++ Uebung1/run_1_3.m
@@ -6,12 +6,12 @@ clc
 close all
 
 disp('Ausgaben der dual1(z,b) Funktion:')
-% dual1(15,8)
-% dual1(42,8)
-% dual1(-77,8)
-% dual1(714,16)
-% dual1(-512,16)
-% dual1(-77,16)
+d1_p15_p8   = dual1(15,8)
+d1_p42_p8   = dual1(42,8)
+d1_m77_p8   = dual1(-77,8)
+d1_p714_p8  = dual1(714,16)
+d1_m512_p16 = dual1(-512,16)
+d1_m77_p16  = dual1(-77,16)
 
 disp('Ausgaben der dual2(z,b) Funktion:')
 % dual2(15,8)

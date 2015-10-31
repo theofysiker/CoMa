57c71bd59a631232fd5c27f4069c8fc600f4c96c
Nicolas Lehmann <mail@nicolaslehmann.de>
2015-10-31 21:26:51 +0100

    run_1_3.m hinzugefügt

---
+++ Uebung1/run_1_3.m
@@ -0,0 +1,22 @@
+%%% Uebung 1 %%%
+
+%%% clean environment
+clear all
+clc
+close all
+
+disp('Ausgaben der dual1(z,b) Funktion:')
+% dual1(15,8)
+% dual1(42,8)
+% dual1(-77,8)
+% dual1(714,16)
+% dual1(-512,16)
+% dual1(-77,16)
+
+disp('Ausgaben der dual2(z,b) Funktion:')
+% dual2(15,8)
+% dual2(42,8)
+% dual2(-77,8)
+% dual2(714,16)
+% dual2(-512,16)
+% dual2(-77,16)
